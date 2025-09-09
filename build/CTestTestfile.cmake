93902e449a6962970c89c8cc80ac9efdb8bc6188
QINGkaka <2220294086@qq.com>
2025-09-09 09:22:29 +0800

    Stop tracking .vs, .vscode, and build directories

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/SepExp/miniBase
-# Build directory: E:/SepExp/miniBase/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
