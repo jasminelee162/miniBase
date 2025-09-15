8218b7f2f419bca6f8d5b95caae062a3f9090e07
chenqing0106 <1065255262@qq.com>
2025-09-15 16:58:21 +0800

    清除之前编译产生上传的冗余文件

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Code/Project_grade3/SX/new/miniBase
-# Build directory: E:/Code/Project_grade3/SX/new/miniBase
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
